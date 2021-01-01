f65b7141afa7395e50f67f6a2c7e8e088f902568
Nguyễn Huy Quang <51860572+DarkW1llow@users.noreply.github.com>
2021-01-01 22:29:24 +0700

    CreateFile as Models

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
+import './assets/base.scss'
+import Main from './CMS'
+import configureStore from './config/configureStore'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = configureStore()
+const rootElement = document.getElementById('root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// store.subscribe(() => console.log(store.getState()));
+
+const renderApp = (Component) => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <Component />
+      </HashRouter>
+    </Provider>,
+    rootElement
+  )
+}
+renderApp(Main)
+if (module.hot) {
+  module.hot.accept('./CMS', () => {
+    const NextApp = require('./CMS').default
+    renderApp(NextApp)
+  })
+}
+serviceWorker.unregister()
