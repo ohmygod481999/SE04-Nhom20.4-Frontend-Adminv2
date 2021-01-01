5fb18a1b0490a7b59cafc5e115c5b702ce325610
nam255199 <56675183+nam255199@users.noreply.github.com>
2021-01-01 23:11:48 +0700

    add Template gốc (k dùng trong việc render, code)
    Template gốc dùng để xem template khi chưa bị chỉnh sửa

---
+++ src/LayoutTemplate/AppLogo/index.js
@@ -0,0 +1,87 @@
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+
+import Hamburger from 'react-hamburgers';
+import Logo from '../../assets/images/1912094611_IZZI-Favicon-32px.png'
+
+import AppMobileMenu from '../AppMobileMenu';
+
+import {
+    setEnableClosedSidebar,
+    setEnableMobileMenu,
+    setEnableMobileMenuSmall,
+} from '../../reducers/ThemeOptions';
+
+class HeaderLogo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false,
+            mobile: false,
+            activeSecondaryMenuMobile: false
+        };
+
+    }
+
+    toggleEnableClosedSidebar = () => {
+        let { enableClosedSidebar, setEnableClosedSidebar } = this.props;
+        setEnableClosedSidebar(!enableClosedSidebar);
+    }
+
+    state = {
+        openLeft: false,
+        openRight: false,
+        relativeWidth: false,
+        width: 280,
+        noTouchOpen: false,
+        noTouchClose: false,
+    };
+
+    render() {
+        let {
+            enableClosedSidebar,
+        } = this.props;
+
+        const {
+        } = this.state;
+
+        return (
+            <Fragment>
+                <div className="app-header__logo">
+                    <div className="logo-src" />
+                    {/* <div id="divLogo" style={{ textAlign: 'center', width: '100%' }} className="mx-auto" >
+                        <img src={Logo} className="logo_src" onClick={() => window.location.href = "#/dashboards/home"} style={{ cursor: "pointer" }} >
+                        </img>
+                    </div> */}
+                    <div className="header__pane ml-auto">
+                        <div onClick={this.toggleEnableClosedSidebar}>
+                            <Hamburger
+                                active={enableClosedSidebar}
+                                type="elastic"
+                                onClick={() => this.setState({ active: !this.state.active })}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <AppMobileMenu />
+            </Fragment>
+        )
+    }
+}
+
+
+const mapStateToProps = state => ({
+    enableClosedSidebar: state.ThemeOptions.enableClosedSidebar,
+    enableMobileMenu: state.ThemeOptions.enableMobileMenu,
+    enableMobileMenuSmall: state.ThemeOptions.enableMobileMenuSmall,
+});
+
+const mapDispatchToProps = dispatch => ({
+
+    setEnableClosedSidebar: enable => dispatch(setEnableClosedSidebar(enable)),
+    setEnableMobileMenu: enable => dispatch(setEnableMobileMenu(enable)),
+    setEnableMobileMenuSmall: enable => dispatch(setEnableMobileMenuSmall(enable)),
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderLogo);
