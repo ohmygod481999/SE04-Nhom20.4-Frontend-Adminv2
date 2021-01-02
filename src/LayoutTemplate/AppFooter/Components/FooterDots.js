df3e8bc7a288a8e9b351149900b89c08f75a3e6b
nam255199 <phuongnam.pro2551999@gmail.com>
2021-01-02 20:32:51 +0700

    Thay doi kich thuoc page trong footer, loi lech page A voi page B

--- src/LayoutTemplate/AppFooter/Components/FooterDots.js
+++ src/LayoutTemplate/AppFooter/Components/FooterDots.js
@@ -1,3 +1,4 @@
+@@ -0,0 +1,431 @@
 import React, { Fragment } from 'react'
 
 import { Nav, Button, NavItem, DropdownItem, Popover } from 'reactstrap'
@@ -25,7 +26,20 @@ import ChatExample from './TabsContent/ChatExample'
 import TimelineEx from './TabsContent/TimelineExample'
 import SysErrEx from './TabsContent/SystemExample'
 
-
+const data55 = [
+  { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+  { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+  { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+  { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+  { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+  { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+  { name: 'Page C', uv: 2000, pv: 6800, amt: 2290 },
+  { name: 'Page D', uv: 4780, pv: 7908, amt: 2000 },
+  { name: 'Page E', uv: 2890, pv: 9800, amt: 2181 },
+  { name: 'Page F', uv: 1390, pv: 3800, amt: 1500 },
+  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+]
 
 const tabsContent = [
   {
